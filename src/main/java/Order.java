85bd7c6ff9929427814a89237e45458a73e3067d
gerald <geraldmontufar@gmail.com>
2025-04-14 14:44:52 +0200

    added Instant timestamp to Order record

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,10 +1,12 @@
+import java.time.Instant;
 import java.util.List;
 
 import lombok.With;
 
 @With
 public record Order(
-        String id,
-        List<Product> products,
-        OrderStatus status) {
+                String id,
+                List<Product> products,
+                OrderStatus status,
+                Instant timestamp) {
 }
