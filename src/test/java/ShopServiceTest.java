c10f0281135bfddd82b27c1116f1383fe34375e7
gerald <geraldmontufar@gmail.com>
2025-04-14 10:21:04 +0200

    added tests for getOrdersWithStatus method

--- src/test/java/ShopServiceTest.java
+++ src/test/java/ShopServiceTest.java
@@ -1,5 +1,6 @@
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -33,5 +34,35 @@ class ShopServiceTest {
 
         // THEN
         assertNull(actual);
+    }
+
+    @Test
+    void getOrdersWithStatus_returnsMatchingProducts_whenStatusIsPresent() {
+        ShopService shopService = new ShopService();
+        List<String> productsIds = List.of("1");
+
+        Order order1 = shopService.addOrder(productsIds);
+        Order order2 = shopService.addOrder(productsIds);
+
+        assertThat(shopService.getOrdersWithStatus(OrderStatus.PROCESSING))
+                .containsExactlyInAnyOrder(order1, order2);
+    }
+
+    @Test
+    void getOrderWithStatus_returnsEmptyList_whenStatusIsNotPresent() {
+        ShopService shopService = new ShopService();
+        List<String> productsIds = List.of("1");
+
+        shopService.addOrder(productsIds);
+
+        assertThat(shopService.getOrdersWithStatus(OrderStatus.COMPLETED)).isEmpty();
+
+    }
+
+    @Test
+    void getOrderWithStatus_returnsEmptyList_whenNoOrdersPresent() {
+        ShopService shopService = new ShopService();
+        assertThat(shopService.getOrdersWithStatus(OrderStatus.PROCESSING)).isEmpty();
+
     }
 }
