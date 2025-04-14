8730482838a2d225a9e56c85496fa5e835d9c8b1
gerald <geraldmontufar@gmail.com>
2025-04-14 09:59:33 +0200

    updated every instance of Order constructor to add an initial OrderStatus

--- src/test/java/ShopServiceTest.java
+++ src/test/java/ShopServiceTest.java
@@ -1,36 +1,37 @@
-import org.junit.jupiter.api.Test;
-
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import org.junit.jupiter.api.Test;
 
 class ShopServiceTest {
 
     @Test
     void addOrderTest() {
-        //GIVEN
+        // GIVEN
         ShopService shopService = new ShopService();
         List<String> productsIds = List.of("1");
 
-        //WHEN
+        // WHEN
         Order actual = shopService.addOrder(productsIds);
 
-        //THEN
-        Order expected = new Order("-1", List.of(new Product("1", "Apfel")));
+        // THEN
+        Order expected = new Order("-1", List.of(new Product("1", "Apfel")), OrderStatus.PROCESSING);
         assertEquals(expected.products(), actual.products());
         assertNotNull(expected.id());
     }
 
     @Test
     void addOrderTest_whenInvalidProductId_expectNull() {
-        //GIVEN
+        // GIVEN
         ShopService shopService = new ShopService();
         List<String> productsIds = List.of("1", "2");
 
-        //WHEN
+        // WHEN
         Order actual = shopService.addOrder(productsIds);
 
-        //THEN
+        // THEN
         assertNull(actual);
     }
 }
