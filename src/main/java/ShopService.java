85bc3181e12a2b6705b12608a6c5d2f12c1ba1d0
gerald <geraldmontufar@gmail.com>
2025-04-14 13:43:36 +0200

    added ProductNotFound expection and throw of said exception when ProductService tries to add an order with an invalid product id

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -7,14 +7,14 @@ public class ShopService {
     private final ProductRepo productRepo = new ProductRepo();
     private final OrderRepo orderRepo = new OrderMapRepo();
 
-    public Order addOrder(List<String> productIds) {
+    public Order addOrder(List<String> productIds) throws ProductNotFoundException {
         List<Product> products = new ArrayList<>();
 
         for (String productId : productIds) {
             Optional<Product> productToOrder = productRepo.getProductById(productId);
             if (productToOrder.isEmpty()) {
-                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
-                return null;
+                throw new ProductNotFoundException("No product with an id of " + productId + " was found.");
+
             }
             products.add(productToOrder.get());
         }
